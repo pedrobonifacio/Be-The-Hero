0cc49516062f756d791f85fa0c25d90edad69652
PedroBoni <pedro.henrique1210@outlook.com>
2020-03-28 12:11:56 -0300

    Front-end do App

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -33,20 +33,19 @@ module.exports={
 
         return res.json({id});
     },
-    async delete(req,res){
+    async delete(req, res){
         const { id } = req.params;
         const ong_id = req.headers.authorization;
-
+    
         const incident = await connection('incidents')
-        .where('id',id)
+        .where('id', id)
         .select('ong_id')
         .first();
-
-        if(incident.ong_id != ong_id){
-            return res.status(401).json({error:'Operation not permitted'});
-        } 
-        await connection('incident').where('id',id).delete();
-
-        return res.status(404).send();
-    }   
+    
+        if (incident.ong_id !== ong_id) {
+          return res.status(401).json({ error: 'Operation not permitted.'})
+        }
+        await connection('incidents').where('id', id).delete();
+        return res.status(204).send();
+      }
 }
